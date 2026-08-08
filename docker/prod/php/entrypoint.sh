601143d55139bfbebdd4bc1dbe1a6a1ba9aa338a
NonSense-U <malekrostum@gmail.com>
2026-08-08 03:52:37 +0300

    Docker storage live volume

--- docker/prod/php/entrypoint.sh
+++ docker/prod/php/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+
+mkdir -p /var/www/html/storage/framework/cache/data \
+         /var/www/html/storage/framework/sessions \
+         /var/www/html/storage/framework/views \
+         /var/www/html/storage/framework/testing \
+         /var/www/html/storage/logs \
+         /var/www/html/bootstrap/cache
+
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-exec "$@"
+
+if [ "$1" = "php-fpm" ]; then
+    exec "$@"
+else
+    exec setpriv --reuid=www-data --regid=www-data --init-groups "$@"
+fi
